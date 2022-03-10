609ec69dface63db777336cde94df7bd1d9a22ef
francelino <fmarcilio1989@gmail.com>
2022-03-09 23:05:47 -0300

    adicionando a lista de fotos

--- src/app/fotos/fotos.service.ts
+++ src/app/fotos/fotos.service.ts
@@ -1,10 +1,11 @@
+import { environment } from './../../environments/environment';
 import { TokenService } from './../autenticacao/token.service';
 import { Fotos } from './fotos';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const API = 'http://localhost:3000';
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
@@ -15,11 +16,8 @@ export class FotosService {
     private tokenService: TokenService
   ) {}
 
-  listaDUsuario(nomeDoUsuario: string): Observable<Fotos> {
+  listaDosuario(nomeDoUsuario: string): Observable<Fotos> {
     const token = this.tokenService.retornaToken();
-    const headers = new HttpHeaders().append('x-access-token', token);
-    return this.httpClient.get<Fotos>(`${API}/${nomeDoUsuario}/photos`, {
-      headers,
-    });
+    return this.httpClient.get<Fotos>(`${API}/${nomeDoUsuario}/photos`);
   }
 }
--- src/app/fotos/list-fotos/list-fotos.component.ts
+++ src/app/fotos/list-fotos/list-fotos.component.ts
@@ -1,15 +1,27 @@
+import { FotosService } from './../fotos.service';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Fotos } from './../fotos';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-fotos',
   templateUrl: './list-fotos.component.html',
-  styleUrls: ['./list-fotos.component.css']
+  styleUrls: ['./list-fotos.component.css'],
 })
 export class ListFotosComponent implements OnInit {
+  fotos!: Fotos;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private fotosService: FotosService
+  ) {}
 
   ngOnInit(): void {
+    this.usuarioService.retornaUsuario().subscribe((usuario) => {
+      const userName = usuario.name ?? '';
+      this.fotosService.listaDosuario(userName).subscribe((fotos) => {
+        this.fotos = fotos;
+      });
+    });
   }
-
 }

d8f19ada1020be8a883cc4324d1bb7b0abeae2df
francelino <fmarcilio1989@gmail.com>
2022-03-09 22:00:19 -0300

    Adicionando o component de fotos

---
+++ src/app/fotos/foto/foto.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+const API = 'http//localhost:3000';
+
+@Component({
+  selector: 'app-foto',
+  templateUrl: './foto.component.html',
+  styleUrls: ['./foto.component.css'],
+})
+export class FotoComponent implements OnInit {
+  private urlOriginal = '';
+
+  @Input() descricao = '';
+
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.urlOriginal = url;
+    } else {
+      this.urlOriginal = `${API}/imgs/${url}`;
+    }
+  }
+
+  get url(): string {
+    return this.urlOriginal;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/fotos/fotos.module.ts
+++ src/app/fotos/fotos.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FotosRoutingModule } from './fotos-routing.module';
 import { ListFotosComponent } from './list-fotos/list-fotos.component';
+import { FotoComponent } from './foto/foto.component';
 
 @NgModule({
-  declarations: [ListFotosComponent],
+  declarations: [ListFotosComponent, FotoComponent],
   imports: [CommonModule, FotosRoutingModule],
 })
 export class FotosModule {}
